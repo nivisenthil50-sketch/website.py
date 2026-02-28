1e9679b3887fca580c79644003d769db70b3357c
nivisenthil50-sketch <nivisenthil50@gmail.com>
2026-02-28 14:45:56 +0530

    Add files via upload

---
+++ sv.py
@@ -0,0 +1,12 @@
+# Simple Python program to add two numbers
+
+# Taking input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the numbers
+sum_result = num1 + num2
+
+# Displaying the result
+print("The sum is:", sum_result)
+
